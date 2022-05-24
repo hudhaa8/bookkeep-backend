aa41c1143f954535a1fee8576ac300415fc12222
hudhamalick <noorulcse@gmail.com>
2022-05-24 18:22:53 +0530

    change

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const router = require("./routes/book-routes")
 const app = express();
 const cors= require("cors")
-//middleware
+const dotenv = require("dotenv");
 
+//middleware
+dotenv.config();
 // app.use("/",(req,res)=>{
 //     res.send("This is our app")
 // })
@@ -14,9 +16,9 @@ app.use("/books", router)   // localhost:5000/books
 
 mongoose
   .connect(
-    "mongodb+srv://user1:Welcome123@cluster0.y1tvm.mongodb.net/book-keeping-app?retryWrites=true&w=majority"
+    process.env.MONGO_URL
   )
   .then(() => console.log("connected to database"))
   .then(()=>{
-      app.listen(5000)
+      app.listen(process.env.PORT || 5000)
   }).catch((error)=>console.log(error))
